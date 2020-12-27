9b5f7bd99e41b795017cd1ea3ff5bd6f1d6b6173
Chris Clouser <waryjack@gmail.com>
2020-12-27 18:51:57 -0500

    enabled basic Dice So Nice! integration

--- module/interaction/EWMessageHelper.js
+++ module/interaction/EWMessageHelper.js
@@ -4,11 +4,12 @@ export class EWMessageHelper {
 
 
         console.warn("Requested Message Template: ", template);
-
+        console.warn("Data: ", data);
         renderTemplate(template, data).then((msg)=>{
             ChatMessage.create({
                 user: game.user._id,
-                type:CONST.CHAT_MESSAGE_TYPES.ROLL,
+                roll: data.roll,
+                type:CHAT_MESSAGE_TYPES.ROLL,
                 speaker: ChatMessage.getSpeaker(),
                 content: msg
             });
